005ea7424b9b52871e9164c9d3e4339924e1fff9
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-03-09 15:25:10 -0500

    js exercise

--- app.js
+++ app.js
@@ -3,8 +3,35 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
+const firstName = "kal";
+let lastName = "shibeshi";
+var age = 26;
 
-// Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first release in ${createdYear} .`);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(
+  `Nike shoe on sale? ${isOnSale}. Their price in size ${selectedSize} is $${price} .`
+);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(
+  `${title} by ${author} is ${pageCount} long. Your bookmark page: ${bookmark}.`
+)
